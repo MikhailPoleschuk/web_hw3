cd5f273a946f7df4f411e1237f5045385e70e9e6
MikhailPoleschuk <113336563+MikhailPoleschuk@users.noreply.github.com>
2023-02-15 09:30:37 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,3 @@
+var c = parseInt(prompt("Введите температуру в градусах Цельсия:"));
+let f=(9 / 5) * c + 32;
+alert(f.toFixed(2)); 
---
+++ 2.js
@@ -0,0 +1,7 @@
+function greeting() {
+    user_name = prompt("Привет, как тебя зовут?");
+
+    alert( 'Привет, ' + user_name + "!");
+}
+
+greeting();
